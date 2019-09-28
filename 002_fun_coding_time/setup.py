40b98bc70053ac02e471ffd08d2e69afafb0b2f6
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2019-09-28 09:00:16 -0400

    Added python package settings

--- 002_fun_coding_time/setup.py
+++ 002_fun_coding_time/setup.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
 # pylint: disable=C0111,W6005,W6100
-from __future__ import absolute_import, print_function
 
 import os
 import re
 import sys
 
-from setuptools import setup
-
-
-app_name = 'paypal'
+from setuptools import setup, find_packages
 
 
 def get_version(*file_paths):
@@ -25,7 +21,8 @@ def get_version(*file_paths):
     raise RuntimeError('Unable to find version string.')
 
 
-VERSION = get_version('apps', app_name, '__init__.py')
+PCKS = find_packages(exclude=['tests', 'tests.*']),
+VERSION = get_version(str(PCKS[0][0]), '__init__.py')
 
 if sys.argv[-1] == 'tag':
     print("Tagging the version on github:")
@@ -35,35 +32,48 @@ if sys.argv[-1] == 'tag':
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 CHANGELOG = open(os.path.join(os.path.dirname(__file__), 'CHANGELOG.rst')).read()
+# Get more https://pypi.org/pypi?%3Aaction=list_classifiers
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Framework :: Django :: 1.8',
+    'Framework :: Django :: 1.9',
+    'Framework :: Django :: 1.10',
+    'Framework :: Django :: 1.11',
+    'Intended Audience :: Developers',
+    'License :: Other/Proprietary License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+],
+
 
 setup(
-    name=app_name,
+    name=str(PCKS[0][0]),
     version=VERSION,
     description="""An REST API that processor the client payments from the Paypal service.""",
     long_description=README + '\n\n' + CHANGELOG,
     author='Leonardo J. Caballero G.',
     author_email='leonardocaballero@gmail.com',
     url='https://bitbucket.org/macagua/edunext-challenge',
-    packages=[
-        app_name,
-    ],
+    packages=PCKS[0],
     include_package_data=True,
     install_requires=[
         "Django>=1.8,<1.11"
     ],
     zip_safe=False,
     keywords='Django edx',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Framework :: Django',
-        'Framework :: Django :: 1.8',
-        'Framework :: Django :: 1.9',
-        'Framework :: Django :: 1.10',
-        'Framework :: Django :: 1.11',
-        'Intended Audience :: Developers',
-        'License :: Other/Proprietary License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-    ],
+    classifiers=CLASSIFIERS,
+    license='Other/Proprietary License',
+    platforms='OS Independent',
 )
